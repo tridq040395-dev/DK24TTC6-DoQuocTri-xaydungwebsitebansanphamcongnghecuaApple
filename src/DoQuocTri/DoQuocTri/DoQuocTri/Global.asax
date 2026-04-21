31042da6c3b3482b7a32e6a1bb7dbf0c968771e5
tridq040395-dev <tridq040395@tvu-onschool.edu.vn>
2026-04-21 19:46:59 +0700

    Tải lên

---
+++ src/DoQuocTri/DoQuocTri/DoQuocTri/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hoatuoi.MvcApplication" Language="C#" %>
---
+++ src/DoQuocTri/DoQuocTri/DoQuocTri/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hoatuoi.Startup))]
+
+namespace Hoatuoi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
